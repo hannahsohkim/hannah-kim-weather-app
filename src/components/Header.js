b2d05c7c0b9dcae1b20b15b00ed0fadb8d30c2d9
Hannah Kim <hannahsohkim@gmail.com>
2019-07-08 21:04:59 -0700

    Create Header component for main app header and intro

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>Enter a city to find out today's weather conditions!</p>
+    </div>
+  )
+}
+
+export default Header;
