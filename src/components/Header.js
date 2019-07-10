13e6aed97cd8cfc2f58d4c34058ceb855a59c0cc
Hannah Kim <hannahsohkim@gmail.com>
2019-07-10 00:47:38 -0700

    Change font in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SubHeader = styled.p`
+  font-family: Avenir
+`;
 
 const Header = () => {
   return (
     <div>
       <h1>United States Weather App</h1>
-      <p>Enter a city anywhere in the US to find out today's weather conditions!</p>
+      <SubHeader>Enter a city anywhere in the US to find out today's weather conditions!</SubHeader>
     </div>
   )
 }
