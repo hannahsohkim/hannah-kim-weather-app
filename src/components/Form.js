b51029967affdcb353f5a6ca6679d630bccaa5ea
Hannah Kim <hannahsohkim@gmail.com>
2019-07-09 23:21:53 -0700

    Edit to use original API and add styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #7979E1;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  padding: 7px;
+  width: 160px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+  &:hover {
+    background-color: #3939D9
+  }
+  &:focus {
+    outline: none !important
+  }
+`
+const Input = styled.input`
+  height: 23px;
+  border-radius: 4px;
+  &:focus {
+    outline: none !important;
+    border: 3px solid #7979E1
+  }
+`
 
 const Form = (props) => {
   return(
-    <Box
-      display="flex"
-      flexDirection="row"
-      justifyContent="flex-start"
-      textAlign="right"
-    >
-      <form onSubmit={props.getWeather}>
-        <Box order={3} m={1}>
-          <label> City: <input type="text" name="city" />
-          </label>
-        </Box>
-
-        <Box order={4} m={1}>
-          <label> Country: <input type="text" name="country" />
-          </label>
-        </Box>
-
-        <Box order={5} m={1}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary">
-            Get Weather
-          </Button>
-        </Box>
-
-      </form>
-    </Box>
+    <form onSubmit={props.getWeather}>
+      <Input type="text" name="city" placeholder="City..."/>
+      <Button type="submit">GET WEATHER</Button>
+    </form>
   )
 }
 
